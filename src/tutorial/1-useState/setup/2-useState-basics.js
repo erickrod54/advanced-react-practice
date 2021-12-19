d1d85072840b57e1adbe3b87725aa75a3c3bee2b
Erick Rodriguez <erickrod54@gmail.com>
2021-12-18 21:53:05 -0500

    toggling between two diferent titles using useState hook and a conditional

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const handleClick = () => {
+
+    /** this way makes a toogle functionality*/
+    if (text === 'ramdom title') {
+      setText('Alohaa')
+    } else {
+      setText('ramdom title')
+    }
+  }
+  /**useState hook, can be set to whatever data 
+   * i need to render
+   */
+  const [text, setText] = useState('ramdom title')
+  return (
+    <>
+     <h1>useState basic example</h1>
+     <h2>{text}</h2>
+    <button 
+           type='button'
+           className='btn'
+           onClick={handleClick}
+           >
+             Change title
+           </button>
+    </> 
+     );
 };
 
 export default UseStateBasics;
