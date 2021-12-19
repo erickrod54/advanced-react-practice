5daf92be4dbbb3a991ebddf47d94c38fcb3b2df0
Erick Rodriguez <erickrod54@gmail.com>
2021-12-19 14:40:24 -0500

    data js file modified

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
+  { id: 1, name: 'Esxie' },
+  { id: 2, name: 'Deb' },
+  { id: 3, name: 'Peter' },
+  { id: 4, name: 'Jannelle' },
+  { id: 5, name: 'Ramon' },
+  { id: 6, name: 'Keesha' },
+  { id: 7, name: 'Terry' },
+  { id: 8, name: 'Laury' },
 ];
