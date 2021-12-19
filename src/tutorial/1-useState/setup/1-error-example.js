db41e2e1c898824ab26ddb57fd5444321a4483c3
Erick Rodriguez <erickrod54@gmail.com>
2021-12-18 21:51:03 -0500

    changing a title using a handler action

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const title = 'ramdom title';
+
+  const handleClick = () => {
+      const title = 'green peppers'
+      console.log(title)
+  }
+
+  return (
+  <>
+       <h2>{title}</h2>
+       <button
+              type='button'
+              className='btn'
+              onClick={handleClick}
+              >
+               Change title 
+             </button>
+  </>
+       );
 };
 
 export default ErrorExample;
