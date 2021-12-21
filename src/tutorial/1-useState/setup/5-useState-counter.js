d43037e7a0024f27e2e051b0fc9dd932b9b49110
Erick Rodriguez <erickrod54@gmail.com>
2021-12-20 20:14:46 -0500

    useState-counter is a counter that useState inline function, and reference handler to reset value

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,56 @@
 import React, { useState } from 'react';
 
+/**useState version 1, is a counter that uses inline
+ * function to decrease and increase and a reference 
+ * handler to reset to the initial value
+ */
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  /**to set up the counter, i set an 'initialValue',
+   * variable, this can be whatever value i need
+   */
+  const initialValue = 0;
+
+  /**destructure my counter in a 'value' and 
+   * 'setValue' */
+  const [ value, setValue ] = useState(initialValue);
+
+  /**this handler make a reference to reset the value
+   * when is called onClick
+   */
+  const resetValue = () =>{
+    setValue(initialValue)
+  }
+  console.log('initial value:', initialValue)
+  
+  return (
+    <>  
+      <h1>useState counter example</h1>
+      <section style={{margin:'4rem 0'}}>
+        <h2>regular counter</h2>
+          <h2>{value}</h2>
+          {/**Increase and Decrease button can be
+           * done also as reference handler, in this
+           * case i done with inline function
+           */}
+            <button 
+                className='btn' 
+                onClick={()=>
+                setValue(value -1)}>
+                  Decrease</button>
+                  {console.log('value:', value)}
+            <button 
+                className='btn' 
+                onClick={()=>
+                setValue(value + 1)}>
+                  Increase</button>
+            <button 
+                className='btn' 
+                onClick={()=>
+                resetValue()}>
+                  Reset</button>
+      </section>
+    </>  
+      );
 };
 
 export default UseStateCounter;
