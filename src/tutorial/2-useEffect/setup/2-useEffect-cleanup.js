2d511cc784cda30c55ba37b271da4f248a1cd739
Erick Rodriguez <erickrod54@gmail.com>
2021-12-26 23:54:57 -0500

    building the cleanup function, and his difference with second argument

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,63 @@
 import React, { useState, useEffect } from 'react';
 
-// cleanup function
-// second argument
+// cleanup function - mostly used for:
+/**
+ *    EventListeners, 
+ *    components appering and dessappearing 
+ *    suscriptions
+ *  
+ * objective of this good practice is avoid
+ * 'Memory leak'
+ */
+// second argument can also used to clean memory, but 
+/** the big approach of 'cleanup function' is that you
+ * can design a function for specific cases
+*/
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  /**this example is about window object size */
+  const [size, setSize ] = useState(window.innerWidth)
+  
+  /**There is something called 'Memory leak' 
+   * always is a good practice to build a 
+   * cleanup function to avoid errors,
+   * mostly with live update effects
+   * like this example of checksize
+  */
+  const checksize = () =>{
+    setSize(window.innerWidth)
+  }
+  /**if i leave it without a cleanup function
+   * i can check the live update by JavaConsole
+   * 'Elements > EventListeners' for this case
+   * of EventListeners
+   */
+  
+  useEffect(() =>{ 
+    /**every time i resize i call, in this case 'Hello World' */
+    console.log('Hello World')
+    window.addEventListener('resize', checksize)
+
+    /**Building cleanup function -i place a return with a 
+     * function that removes the EventListener- */
+    return () => {
+      console.log('cleanup function -i clean Hello World :) -')
+      window.removeEventListener('resize', checksize)
+    }
+  })
+  
+
+
+
+  return (
+    <>
+      <h2>useEffect cleanup Function</h2>
+      <h2>Window</h2>
+      <h2>{size} PX</h2>
+    </>
+      );
 };
+
+
 
 export default UseEffectCleanup;
