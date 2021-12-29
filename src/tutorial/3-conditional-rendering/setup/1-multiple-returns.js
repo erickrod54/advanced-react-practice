1eb3ee757bc7170b7ee9ede7c3ff3222b89c42b0
Erick Rodriguez <erickrod54@gmail.com>
2021-12-28 21:21:02 -0500

    first approach for multiple return, for conditional rendering - loading state example -

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
+/**In this example i develop a Loading state */
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+
+  /**Multiple returns is a usefull way to conditionally rendering
+   * and develop features like a 'loading' feature for this example,
+   * or rendering 'conditionally a component' that makes really
+   * dynamic components rendering
+   */
+
+  /**first i destructure 2 states for useState so i be 
+   * able to switch between true and false*/
+  const [ loading, setLoading ] = useState(true);
+
+  if (loading) {
+    return <h2>Loading ...</h2>;
+  } 
+  
+  /**i can hold a whole app here and render it conditionally */
+  return (
+  <>
+    <h1>Multiple Returns</h1>
+
+    <div className='container'>
+      <h2>Conditional Renderering</h2>
+
+
+    </div>
+
+  </>  
+    );
 };
 
 export default MultipleReturns;
