36c5b15d67a4df8c1e2ab32e1432425ec901db3f
Erick Rodriguez <erickrod54@gmail.com>
2022-01-22 17:21:26 -0500

    Forms Basics version 1 - Features: building basic form, and  controlled inputs behavior

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -5,8 +5,78 @@ import React, { useState } from 'react';
 // React
 // value, onChange
 
+/**-------covered in this version------------ */
+
+/** Forms Basics version 1 - feautures: 
+ * --------->Build a Basic Form to control their 
+ *          inputs.
+ * --------->controlled input behavior( 
+ *              -> to make it behave by the 
+ *                requirement that i need. 
+ *              ->to place it whereever i need it,
+ * )*/
+
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  /**with this reference function trigger a loopback
+   * test to return the behavior that i want from the 
+   * form -> onSubmit={handleSubmit} 
+   * 
+   *  Note: the parameter 'e' is used to target the 
+   *  inputs
+   * 
+   * */
+
+  const handleSubmit = (e) => {
+    /**this line is very important prevents the 
+     * default behavior to handle forms, and let
+     * me take control of the forms*/
+    e.preventDefault();
+    /**doing the previous line, hello world will
+     * be prompted doing the 'behavior' test 
+     * sucessful*/
+    console.log('hello world')
+  }
+
+  return (
+  <>
+    <h2>controlled inputs</h2>
+    <arcticle>
+      {/** styles applied:
+       *          form--> the form container-where 
+       *                  the inputs are-
+       *  form-control--> style for the input itself
+      */}
+
+      {/**i can control where to place and trigger the
+       * action: 
+       *        --->from the form with onSubmit
+       *        --->from the button with onClick
+       * */}
+      <form className='form' onSubmit={handleSubmit}>
+        {/**first input for the name */}
+        <div className='form-control'>
+          <label htmlFor='firstName'>Name:</label>
+          <input 
+            type='text' 
+            id='firstName' 
+            name='firstName'/>
+        </div>
+        {/** second input for the email*/}
+        <div className='form-control'>
+          <label htmlFor='email'>Email:</label>
+          <input 
+            type='text' 
+            id='email' 
+            name='email' />
+        </div>
+        <button type='submit'>add person</button>
+      </form>
+    </arcticle>
+  
+  </>
+    
+  );
 };
 
 export default ControlledInputs;
