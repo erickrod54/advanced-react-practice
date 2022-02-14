db9211d3de290575ff8e14d811c6aeac42159631
Erick Rodriguez <erickrod54@gmail.com>
2022-02-14 14:25:54 -0500

    Modal app version 5 - Features: drilling modalContent from index js to Modal js and render it

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
 
-/**Modal app version 2 - Modal Component - Features:
+/**Modal app version 5 - Modal Component - Features:
  * 
  *      -->drilling 'modalContent' from:
  *                  index.js --> Modal.js
  *         and render it
  *      
  */
-const Modal = ({modalContent}) => {
+const Modal = ({modalContent, closeModal }) => {
+  
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal()
+    }, 3000)
+  })
+
   return (
   <>
     <div className='modal'>
