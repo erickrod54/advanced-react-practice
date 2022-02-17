b4a4e79a19d72ced8874a00746a128bfd3ece425
Erick Rodriguez <erickrod54@gmail.com>
2022-02-16 21:39:41 -0500

    Prop-drilling app version 1 - App js file - Features: setting up prop-drilling practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/6-useReducer/setup'
-
-/**Modal app version 1 - App js - 
- * 
- *   ---> the way that 'Setup' is imported:
- * 
- *      './tutorial/6-useReducer/setup'
- * 
- *      Works because is in 'setup' a file named
- *      as 'index', so wil be displayed as default 
- */
+import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling'
 
 function App() {
   return (
