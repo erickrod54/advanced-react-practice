d796f0ed9e8f381223fd74b8866d5bca7f34aa16
Erick Rodriguez <erickrod54@gmail.com>
2022-03-18 17:52:53 -0400

    Custom Hook app version 1 - fecth example js - Features: Implementing useFetch custom Hook

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './2-useFetch'
+/**
+ * Custom Hook app version 1 - fetch example js - Fetaures:   
+ *              --->Implementing 'useFetch' custom Hook
+ * 
+ * Note: the propouse of custom Hook is to rehuse functionality;
+ * In this case i will set up to rehuse a fetch already done 
+ * functionality for multiple projects
+ * 
+ * -----the use of the word 'use' is essential in order to
+ * -----custom hook to work--example: 'useFecth, useRemove, etc'
+ * 
+ * Custom Hook --> to rehuse functionality
+ * 
+ * Components --> to rehuse 'JSX'
+ */
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  /**here i implement the custom hook */
+  const { loading, products } = useFetch(url)
   console.log(products)
+  
   return (
     <div>
+      <h2>Custom Hook - fetch</h2>
       <h2>{loading ? 'loading...' : 'data'}</h2>
     </div>
   )
