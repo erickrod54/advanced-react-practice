acc4082d7e42e0c2d097783439548f66843b6aba
Erick Rodriguez <erickrod54@gmail.com>
2022-03-19 11:19:19 -0400

    Custom Hook app version 1 - useFetch Hook - Features: Building the useFetch custom Hook

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,27 @@
 import { useState, useEffect } from 'react';
+/**
+ * Custom Hook app version 1 - useFetch Hook - Fetaures:   
+ *              --->Building the 'useFetch' custom Hook
+ * 
+ * Note: i return the props, and i pass the url
+ */
 
-export const useFetch = () => {};
+/**here i pass the url */
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    const response = await fetch(url)
+    const products = await response.json()
+    setProducts(products)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getProducts()
+    /**i removed form the dependency, it will be passed */
+  }, [])
+  /**i set  up a return for the props that generate the fetch */
+  return {loading, products}
+};
