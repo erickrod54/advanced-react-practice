245778cdef246856486c2bffc80874320e746251
Erick Rodriguez <erickrod54@gmail.com>
2022-03-22 22:00:01 -0400

    PropTypes app version 4 - Product Component - Features: Applying Short Circuit operator Or to price and name, condition of existence for image and Short Circuit or to set defualtImage

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,62 +3,70 @@ import PropTypes from 'prop-types'
 /**with this import i handle the missing image */
 import defaultImage from '../../../assets/default-image.jpeg';
 
-/** PropTypes app version 3 - 'Product' Component - Features:
- *          -->Importing 'PropTypes' package.
- *          -->Setting PropTypes in the Component.
- *          -->Setting default values using PropTypes 
- *             for the missing data:
- *                        --> 'image'
- *                        --> 'price'
+/** PropTypes app version 4 - 'Product' Component - Features:
+ * 
+ *          -->Applying Short Circuit operator 'Or' to set 
+ *             default value for 'price'. 
+ * 
+ *          -->Using an 'url' variable to condition the existence
+ *             pf 'image' and 'image.url'.
+ * 
+ *          -->Applying Short Circuit operator 'Or' to set default
+ *             value for 'image'.
+ * 
+ *          -->Applying Short Circuit operator 'Or' to set default
+ *             value for 'name'.
  * 
  * Note: first i'll set PropTypes in the according the props
  * type, second i set default values, and then i can uncomment
  * JSX and render to watch default values. There is two 
  * approaches:
  * 
- * -------------This approach is the first--------------------
+ * -------------This approach is the second--------------------
  * 
  *    --> PropTypes -> code more readable.
  *    --> Short Circuit operator and props -> code less readable.
+ * 
+ * keyboard shortcut - rafcp -this will set to test PropTypes-
  */
+
 
 /**here i destructure 'Product' props */
 const Product = ({image, name, price}) => {
   /**then when y prompted i checkout undefined values */
+
+  /**Here i made Short Circuit Opeartor - cheking if
+   * image exist and image.url*/
+  const url = image && image.url 
   
   console.log(image,name,price)
   return (
   <>
-  {/**Third step */
-  /**----after set up PropTypes and set default values
-  * step i uncomment and i can visualize the data */}
     <article className='product'>
-      <img src={image.url} alt={name}/>
+      {/**Here i condition the 'url' or 'defaultImage'*/}
+      <img src={url || defaultImage} alt={name || 'default name'}/>
       <h4>{name}</h4>
-      <p>${price}</p>
+      {/**Here i condition the price if exists or '3.99' 
+       * default value*/}
+      <p>${price || 3.99 }</p>
     </article> 
   </>
   
   );
 };
 
-/**First step */ 
-/**Here i set up PropTypes in the Component acording with
- * the data type of each prop, image an object, name as string
- * price as number every each required */
 Product.propTypes = {
   image: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired
 }
 
-/**Second step */
-/**I set defualt values */
-Product.defaultProps = {
-  name: 'default name',
-  price: 3.99,
-  image:defaultImage
-}
+/**first i comment the first approach */
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image:defaultImage
+// }
 
 
 export default Product;
