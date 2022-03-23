0d57f3839e43c017c714a0ed52f9c7924805cedb
Erick Rodriguez <erickrod54@gmail.com>
2022-03-23 17:18:55 -0400

    React-router app version 2 - Error js file - Features: Importing and using Link Component to navigate back from Error Component to Home Component

--- src/tutorial/11-react-router/setup/Error.js
+++ src/tutorial/11-react-router/setup/Error.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+/**
+ * React-router app version 2 - Error js file - Features:
+ * 
+ *              --> Importing and using 'Link' Component 
+ *                  to navigate back from Error Component
+ *                  to 'Home' Component
+ * 
+ * Note: 'Link' is the Component recommended instead of 'a'
+ * html tags
+ */
+
 const Error = () => {
   return (
     <div>
       <h1>Error Page</h1>
+      <Link to='/' className='btn'>Back Home</Link>
     </div>
   );
 };
