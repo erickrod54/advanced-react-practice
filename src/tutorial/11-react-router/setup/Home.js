f4317d073ecea2c1d1ca264be9cc99dfa1eda572
Erick Rodriguez <erickrod54@gmail.com>
2022-03-22 22:08:41 -0400

    React-router app version 1 - Home Component - Features: Setting up a message for Home Page

--- src/tutorial/11-react-router/setup/Home.js
+++ src/tutorial/11-react-router/setup/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+/**
+ * React-router app version 1 - Home Component - Features:
+ * 
+ *              --> Setting up a Meesage for the 'Home'
+ *                  Page 
+ * 
+ * Note: Set 'exact' keyword is very important if i dont want 
+ * to display others 'Routes' that will match with 'Home' - '/' 
+ * Route -the forward slash is the domain, this case localhost
+ * but in a domain will be the root domain 'example.com'-
+ */
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      {/**here i set the message */}
+      <p>This is the Home Page</p>
     </div>
   );
 };
