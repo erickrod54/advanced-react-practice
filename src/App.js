d48fd1c4e4de58f88261d17b3bcfa577bf1c65ed
Erick Rodriguez <erickrod54@gmail.com>
2022-03-22 22:04:06 -0400

    Setting up React-router app version 1 - index js - Features: Using Router to wrap the app, setting Routes to render every Component, Set exact keyword for Home Component path, Setting up Error Component and Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/10-prop-types/setup/'
+import Setup from './tutorial/11-react-router/setup/'
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
     <h2>Advanced React Concepts</h2>
     <div className='container'>
       <Setup />
-  
+   
     </div>
     </>
     
