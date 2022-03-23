e7bd87906d9f0b03ec701171d3a0765964b0d0c3
Erick Rodriguez <erickrod54@gmail.com>
2022-03-23 17:14:26 -0400

    React-router app version 2 - index js - Features: Importing and using link Component to create a use-experience navigation

--- src/tutorial/11-react-router/setup/Navbar.js
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+/**
+ * React-router app version 2 - index js - Features:
+ * 
+ *              --> Importing and using 'Link' Component 
+ *                  to create a use-experience navigation.
+ * 
+ * Note: 'Link' is the Component recommended instead of 'a'
+ * html tags
+ */
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+  <>
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link> 
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/people'>People</Link>
+        </li>
+      </ul>
+    </nav>
+  
+  </>
+  
+  );
 };
 
 export default Navbar;
