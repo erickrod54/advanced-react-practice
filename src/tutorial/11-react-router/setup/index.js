f1a3c17a0b53a6b6425728fbb35965aca79d68b1
Erick Rodriguez <erickrod54@gmail.com>
2022-03-22 22:11:48 -0400

    React-Router app version 1 - index js - Features: Using Router to wrap the app, Setting Routes to render every Component, Set exact path for Home Component, Setting up Error Component and Switch

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -9,8 +9,45 @@ import Error from './Error';
 import Person from './Person';
 // navbar
 import Navbar from './Navbar';
+/**
+ * React-router app version 1 - index js - Features:
+ * 
+ *              --> Using 'Router' to wrap the app.
+ *              --> Setting 'Routes' to render every Component.
+ *              --> Set 'exact' keyword for 'Home' Component path.
+ *              --> Setting up 'Error' Component and 'Switch'
+ * 
+ * Note: Set 'exact' keyword is very important if i dont want 
+ * to display others 'Routes' that will match with 'Home' - '/' 
+ * Route -the forward slash is the domain, this case localhost
+ * but in a domain will be the root domain 'example.com'-
+ * 
+ * To start and set 'react-router' is always a good practice to 
+ * make it in a root component so the 'Routes' will have access 
+ * to all the Components
+ */
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return(
+  <>
+  <Router>
+    {/**As '*' matches with all the 'Routes', 'Switch' will
+     * show me 'Error' Component without the match 'Routes'*/}
+    <Switch>
+        <Route exact path='/'><Home /></Route>
+        <Route path='/about'><About/></Route>
+        <Route path='/people'><People/></Route>
+        <Route path='/person'><Person/></Route>
+
+        {/**the star '*' means all routes that match
+         * if i try to acces to a route that does not exist 
+         * for '/adson' will throw the Error Component
+        */}
+
+       <Route path='*'><Error/></Route>
+    </Switch>
+  </Router>  
+  </>
+  )
 };
 
 export default ReactRouterSetup;
