d4db6ed2c31fae15facdd626188099d0e2f0e228
Erick Rodriguez <erickrod54@gmail.com>
2022-03-24 18:04:50 -0400

    React-router app version 3 - People Component - Features: Setting up dynamicly the link for Person Component

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import { data } from '../../../data';
 import { Link } from 'react-router-dom';
+/**
+ * React-router app version 3 - People Component - Features:
+ * 
+ *              --> Setting up 'dinamicly' the link for 
+ *                  Person' Component.
+ * 
+ * Note: 'People' Component is a list that contains each single
+ * 'Person' Component
+ * 
+ * The link is set dinamicly -because the name can be whatever 
+ * i want, thats why is 'dynamic'- to generate the link that 
+ * will display the detail of 'Person' 
+ * 
+ */
 const People = () => {
   const [people, setPeople] = useState(data);
   return (
@@ -10,6 +24,10 @@ const People = () => {
         return (
           <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            {/**here i set the link dinamicly */}
+            <Link to={`/person/${person.id}`}>
+              Know more
+            </Link>
           </div>
         );
       })}
