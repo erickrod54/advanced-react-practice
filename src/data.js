1d249eb50c5dbdd4747593bbb389f77e32f43429
Erick Rodriguez <erickrod54@gmail.com>
2022-03-25 18:51:36 -0400

    React-router app version 3 - data js - Features: Adding a position prop with its value

--- src/data.js
+++ src/data.js
@@ -1,10 +1,19 @@
+/**
+ * React-router app version 3 - data js - Features:
+ * 
+ *              --> Adding a position prop with its value
+ * 
+ * Note: I can add the info that i need and make use of
+ * 'id' and 'useParams' to access it from where i need it,
+ * for this particular case is 'Person' Component
+ */
 export const data = [
-  { id: 1, name: 'Esxie' },
-  { id: 2, name: 'Deb' },
-  { id: 3, name: 'Peter' },
-  { id: 4, name: 'Jannelle' },
-  { id: 5, name: 'Ramon' },
-  { id: 6, name: 'Keesha' },
-  { id: 7, name: 'Terry' },
-  { id: 8, name: 'Laury' },
+  { id: 1, name: 'Esxie', position: 'Surge Tech' },
+  { id: 2, name: 'Deb', position: 'Surge Lead'},
+  { id: 3, name: 'Peter', position:'Robo Tech' },
+  { id: 4, name: 'Jannelle', position: 'Surge Tech'},
+  { id: 5, name: 'Ramon', position: 'Surge Tech'},
+  { id: 6, name: 'Keesha', position: 'Surge Tech'},
+  { id: 7, name: 'Terry', position: 'Robo Tech'},
+  { id: 8, name: 'Laury', position: 'Surge Tech'},
 ];
