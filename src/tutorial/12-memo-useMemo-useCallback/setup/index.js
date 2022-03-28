b2d06818244536b8fe80ec82e2f63ff851054f57
Erick Rodriguez <erickrod54@gmail.com>
2022-03-28 18:49:27 -0400

    React.memo and useCalback app version 3 - index js - Features: Building calculateMostExpensive to get the value of most expensive item, Implementing useMemo hook to calculateMostExpensive and memoize the single value resulting

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,76 +1,80 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import { useFetch } from '../../9-custom-hooks/setup/2-useFetch'
 
 /**
- * React.memo and useCallback app version 2 - index js - Features:
+ * React.memo and useCallback app version 3 - index js - Features:
  * 
- *                  --> Building an 'AddToCart' functionality
- *                      and passing it top-down as a prop to 
- *                     'SingleProduct'
+ *                   --> Building 'calculateMostExpensive'
+ *                       to get the value of most Expensive
+ *                       item.
  * 
- *                  --> Implementing 'useCallback' hook to
- *                      fix the re-render triggered by 
- *                      'AddToCart'
+ *                   --> Implementing 'useMemo' hook to 
+ *                      'calculateMostExpensive' and memoize
+ *                       the single value resulting.
  * 
- * Note: Memoizing renders can be split in two:  
+ * Note: 'useMemo' hook deals memoizing a value, when i test
+ * the function 'calculateMostExpensive', it re-renders in order
+ * to re-build the function that calculates the values over and
+ * over that take computation resources resulting expensive.
  * 
- * -----(like was for 'product and count')-----
- *      for --> states --> 'React.memo'
+ *        React.memo    --> state or props   
+ *        useCallbacks  --> props related with functionality
+ *                          or can memoize a whole functionality
  * 
- * -----(like it is for 'AddToCart' prop)-----
- *      for --> props --> 'useCallback'
+ *        useMemo       --> Memoize a single value
  * 
- * every time props or state changes, component re-renders
- * remenber props can be a value or functionality
- * 
- * In this version 'AddToCart' increases 'cart' state so
- * will trigger a re-render (this time with an action
- * -'AddToCart' by  'cart' state changes-)  
+ * --every time props or state changes, component re-renders--
  */
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
+/**Here i build 'calculateMostExpensive' functionality
+ * to calculate the most expensive item
+*/
+const calculateMostExpensive = (data) => {
+  /**the test to verify that indeed re-renders */
+  console.log('Hello World')
+  return data.reduce((total,item)=>{
+    const price = item.fields.price;
+    if (price >= total) {
+      total = price;
+    }
+    return total
+  }, 0)/100
+}
+
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
-  /**here i build a state for 'cart' */
   const [cart, setCart] = useState(0);
+  
 
-  /**---to use 'useCallback' hook i wrap the functionality
-   *---and in this case i set 'cart' as a dependency cause
-   *---it is what is triggering re-render for 'BigList'
-   */
-
-  /**First - i build the 'AddToCart' fuctionality*/
   const AddToCart = useCallback(() => {
     setCart(cart + 1)
-    /**here i set cart as dependency so when cart state
-     * changes only, then will create this functionality*/
   }, [cart])
 
-  /**--next steps will be pass top-down the 'AddToCart' prop--*/
-  /**----------to get 'SingleProduct' Component------------- */
+const mostExpensive = useMemo(() => calculateMostExpensive(products),
+[products])
+
   return (
     <>
       <h1>Count : {count}</h1>
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      {/**here i render the counter */}
+  
       <h1 style={{marginTop: '3rem'}}>
         Cart: {cart}
       </h1>
-      {/**Second - i pass 'AddToCart' prop throught 'BigList'*/}
+      {/**here i make the call over the products */}
+      <h1>Most Expensive: ${mostExpensive}</h1>
       <BigList products={products} AddToCart={AddToCart}/>
     </>
   )
 }
 
-/**Third - i destructure 'AddToCart' to pass throught
- * 'BigList' Component
-*/
 const BigList = React.memo(({ products, AddToCart }) => {
   useEffect(() => {
     console.log('big list called')
@@ -80,7 +84,6 @@ const BigList = React.memo(({ products, AddToCart }) => {
       {products.map((product) => {
         return <SingleProduct 
                   key={product.id} {...product}
-/**Fourth - i pass 'AddToCart' prop throught 'SingleProduct'*/
                   AddToCart={AddToCart}
                   ></SingleProduct>
       })}
@@ -88,9 +91,6 @@ const BigList = React.memo(({ products, AddToCart }) => {
   )
 })
 
-/**Fifth - i destructure 'AddToCart' to pass throught
- * 'SingleProduct' Component
-*/
 const SingleProduct = ({ fields, AddToCart }) => {
   useEffect(() => {
     console.count('single item called')
@@ -103,9 +103,7 @@ const SingleProduct = ({ fields, AddToCart }) => {
     <article className='product'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
-      <p>${price}</p>
-{/**Sixth - being destructured 'AddToCart' i build the
- * 'button' to simulate adding to the cart */}      
+      <p>${price}</p>     
       <button onClick={AddToCart}>add to cart</button>
     </article>
   )
