82640519b79c46d73cbb293137853d912f2c8716
Erick Rodriguez <erickrod54@gmail.com>
2022-04-04 19:37:10 -0400

    Setting up React-router-v6 practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Setup from './tutorial/12-memo-useMemo-useCallback/setup/'
-
+import Setup from './tutorial/13-react-router-v6/setup/'
+/**
+ * Setting up --> react-router-v6 practice
+ */
 function App() {
   return (
     <>
