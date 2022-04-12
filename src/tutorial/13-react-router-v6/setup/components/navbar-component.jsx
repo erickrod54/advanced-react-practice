b0d5e648bc3c8c4a709ab857fbc32408224e41ec
Erick Rodriguez <erickrod54@gmail.com>
2022-04-12 17:56:36 -0400

    React-routerv6 Products app version 4 - NavBar Component - Features: Implementing NavLinks Component

--- src/tutorial/13-react-router-v6/setup/components/navbar-component.jsx
+++ src/tutorial/13-react-router-v6/setup/components/navbar-component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavLinks } from "./index.styles";
 
-/**React-routerv6 Products app version 3 - 'NavBar' Component- 
+/**React-routerv6 Products app version 4 - 'NavBar' Component- 
  * Features:
  * 
- *              --> Building 'NavBar' Component to set
- *                  navigation links. 
+ *              --> Implementing Navlinks Component. 
  * 
  * Notes: this component will be use for 'SharedLayout' Component
  * 
@@ -15,10 +15,12 @@ const NavBar = () => {
 
     return(
         <>
-            <nav className='nav-links'>
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/products'>Products</Link>
+            <nav >
+                <NavLinks>
+                    <Link to='/home'>Home</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/products'>Products</Link>
+                </NavLinks>
             </nav>
         </>
     )
