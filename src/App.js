907a857379296e571182373b0226175fb7f77766
Erick Rodriguez <erickrod54@gmail.com>
2022-04-13 22:20:49 -0400

    React-routerv6 Products app version 4 - App js file - Features: Importing and Placing GlobalStyles Component to set this styles to the Products app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Setup from './tutorial/13-react-router-v6/setup/'
-/**
- * Setting up --> react-router-v6 practice
+import Setup from './tutorial/13-react-router-v6/setup/';
+import GlobalStyles from './global.styles';
+
+/**React-routerv6 Products app version 4 - 'App.js' file - 
+ * Features:
+ * 
+ *              -->Importing and Placing 'GlobalStyles'
+ *                 Component.
+ * 
+ * Notes: 'GlobalStyles' Component set Global Styles instead
+ * of using 'index.css' style sheet. This way i'll be able to
+ * use without any conflict the styles for 'React-routerv6 
+ * Products app'
+ * 
  */
 function App() {
   return (
     <>
     <h2>Advanced React Concepts</h2>
     <div className='container'>
+      <GlobalStyles/>
       <Setup />
    
     </div>
