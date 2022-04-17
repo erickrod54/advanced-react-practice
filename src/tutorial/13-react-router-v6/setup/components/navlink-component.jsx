104d49c0d6cbaf08b28f8fb5c3bd51ae0caafc88
Erick Rodriguez <erickrod54@gmail.com>
2022-04-17 13:36:13 -0400

    React-routerv6 Products app version 7 - NavLinkComponent - Features: Building a /Login NavLink to set navigation to login Component

--- src/tutorial/13-react-router-v6/setup/components/navlink-component.jsx
+++ src/tutorial/13-react-router-v6/setup/components/navlink-component.jsx
@@ -2,32 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { NavLinks } from "./index.styles";
 
-/**React-routerv6 Products app version 5 - 'NavLinkComponent' - 
+/**React-routerv6 Products app version 7 - 'NavLinkComponent' - 
  * Features:
  * 
- *              --> Implementing 'Navlink' Component from 
- *                  'react-router-dom'.
- *  
- *              --> First way to apply 'isActive' - Implementing 
- *                  an 'Inline Style' to set an '{isActive}' 
- *                  class to '/home'.
+ *              --> Building '/login' 'NavLink' to set navigation
+ *                  to 'Login' Component.
  * 
- *              --> Second way to apply 'isActive' - Implementing
- *                  a className on '/about'.
- * 
- *              --> Third way to apply 'isActive' - Implementing
- *                  a Customize Component Class.
- * 
- * Notes: 'NavLink' Component 
- * 
- *      '{isActive}' is a prop from NavLink Component
- * 
- *------i use brackets because i'm destructuring from
- *------NavLink Object-Component---------------------
- * 
- * after the return i can style whatever prop from the 'NavLink'
- * that i want, this case the 'color', but can be the 'font',
- * 'font-size', 'font-weight' and so on.
+ * Notes: this navigation will be displayed with the rest of 'NavLinks'.
  */
 
 const NavLinkComponent = () => {
@@ -55,6 +36,16 @@ const NavLinkComponent = () => {
                          }>About</NavLink>
 
                     <NavLink to='/products' >Products</NavLink>
+
+                    <NavLink 
+                        to='/login' 
+                        style={({isActive}) => {
+                        return { color: isActive ? 
+                            '#A10000'
+                             : 
+                             '#F4BB44'}
+                    }}>Login</NavLink>     
+                    
                 </NavLinks>
             </nav>
         </>
