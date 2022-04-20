a4bed71ce2dcf9de47e55aab1cdfa7cb7dc36632
Erick Rodriguez <erickrod54@gmail.com>
2022-04-20 18:00:37 -0400

    React-routerv6 app version 8 - index js - Features: Implementing ProtectedRoute on Dashboard, Implementing Nested Routes to place a SharedProductsLayout and SingleProduct

--- src/tutorial/13-react-router-v6/setup/index.js
+++ src/tutorial/13-react-router-v6/setup/index.js
@@ -7,22 +7,27 @@ import Home from './pages/home-component';
 import About from "./pages/about-component";
 import Products from "./pages/products-component";
 import Error from "./pages/error-component";
+
+/**this are the shared layouts for the app and for the products*/
 import SharedLayout from "./pages/shared-layout.component";
+import SharedProductsLayout from "./pages/shared-layout-products-component";
+
 import SingleProduct from "./pages/single-product-component";
 import DashBoard from "./pages/dashboard-component";
+import ProtectedRoute from "./pages/protected-route-component";
 import Login from "./pages/login.component";
 
-/**React-routerv6 Products app version 7 - index js - Features:
+/**React-routerv6 Products app version 8 - index js - Features:
  * 
- *              --> Building States for Authentication 'user', 'setUser'.
- *              
- *              --> Building 'Login' Route passing a 'setUser' prop.
+ *              --> Implementing 'ProtectedRoute' on 'Dashboard'.
  * 
- *              --> Building 'Dashboard' Route passing a 'user' prop. 
+ *              --> Implementing Nested Routes to place a 
+ *                 'SharedProductsLayout' on 'Products' and 
+ *                 'SingleProduct'.
  * 
- * Notes: 'setUser' will be passed as props to 'Login' to build 
- * authentication process and 'user' will be passed as props to 'DashBoard'
- * to build the after authentication process.
+ * Notes: 'ProtectedRoute' is a component that restrict access to
+ * their children routes, in this case i'll apply it to
+ * 'Dashboard'.
  */
 
 const Index = () => {
@@ -38,22 +43,36 @@ const Index = () => {
                     <Route path='/' element={<SharedLayout />}>
                        <Route index path='/home' element={<Home />}/>
                        <Route path='/about' element={<About />}/>
-                       <Route path='/products' element={<Products />}/>
 
-                       {/**Here i build 'URL Params' for every single
-                        * product*/}
-                       <Route 
-                            path='/products/:productId' 
-                            element={<SingleProduct />}/>
+                       {/** '/products' belongs to '<SharedProductsLayout />' */}                    
+                       <Route path='/products' element={<SharedProductsLayout />}>
+                           {/**As this two are children they already 
+                            * have '/products'*/}
+
+                            {/** '<Products />' is set as 'index'*/}
+                           <Route index element={<Products />}/>
+                           {/**i took off '/products' cause is sharing layout*/}
+                            <Route 
+                                path=':productId' 
+                                element={<SingleProduct />}/>
+                       </Route>
+                       
+
 
                         {/**'setUser' for 'Login'*/}
                         <Route 
                             path='/login' 
                             element={<Login setUser={setUser}/>}/>
-                        {/**'user' for 'Dashboard'*/}                                                            
+                            
+                        {/**'user' for 'Dashboard'*/}
+                        {/**i wrap 'DashBoard' with 'ProtectedRoute'*/}                                                            
                         <Route 
                             path='/dashboard' 
-                            element={<DashBoard user={user}/>}/>    
+                            element={
+                            <ProtectedRoute user={user}>
+                                <DashBoard user={user}/>
+                            </ProtectedRoute>
+                            }/>    
                        <Route path='*' element={<Error />}/>
                     </Route>
                </Routes>
