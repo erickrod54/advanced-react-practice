c8a7f32b8d01fc47df1a199e68d498d1b7a02a15
Erick Rodriguez <erickrod54@gmail.com>
2022-04-21 17:19:35 -0400

    AxiosApp version 1 - axios-app file - Features: Building AxiosApp Component

---
+++ src/tutorial/14-axios-react-practice/setup/axios-app.jsx
@@ -0,0 +1,31 @@
+import Title from './components/Title';
+
+/**AxiosApp app version 1 - 'axios-app' file - Features:
+ * 
+ *              --> Building 'AxiosApp' Component.
+ * 
+ * Notes: Axios is a library that manages the 
+ * fetching data from ... and sending data to... 
+ * ------------ a server---------------------
+ * 
+ * Quote:
+ * 
+ *  "Axios is a simple promise based HTTP client for
+ *  the browser and node.js. Axios provides a simple to use
+ *  library in a small package with a very extensible interface."
+ * 
+ * reference: https://axios-http.com/
+ * 
+ * this component is a test for axios
+ */
+
+const AxiosApp = ()  => {
+
+  return (
+    <main>
+      <Title />
+    </main>
+  );
+}
+
+export default AxiosApp;
