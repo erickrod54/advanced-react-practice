5489ae3c7db632a6a469e1f9e99667691ee1a1c8
Erick Rodriguez <erickrod54@gmail.com>
2022-04-24 11:43:12 -0400

    Axios app version 2 - headers file - Features: Implementing axios second argument to render headers prop from axios object

--- src/tutorial/14-axios-react-practice/setup/examples/2-headers.js
+++ src/tutorial/14-axios-react-practice/setup/examples/2-headers.js
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+/**AxiosApp app version 2 - 'headers' file - Features:
+ * 
+ *              --> Implementing 'axios' second argument
+ *                  to render 'headers' prop from axios
+ *                  object.
+ * 
+ * Notes: from the 'url' API i access to 'application/json'
+ * in order to render 'data.joke'
+ */
+
 const url = 'https://icanhazdadjoke.com/';
 // Accept : 'application/json'
 
@@ -8,7 +18,20 @@ const Headers = () => {
   const [joke, setJoke] = useState('random dad joke');
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      /**here i set the axios prop 'headers' to get
+       * the 'jokes' data from the API
+      */
+      const { data } = await axios(url, {
+        headers:{
+          Accept: 'application/json'
+        }
+      });
+      setJoke(data.joke)
+      console.log(data.joke) 
+    } catch (error) {
+      console.log(error.response)
+    }
   };
 
   return (
