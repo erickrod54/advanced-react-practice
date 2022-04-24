9f00ed5bfd35bf75d317f6e2b692ba96e2896852
Erick Rodriguez <erickrod54@gmail.com>
2022-04-24 15:43:59 -0400

    AxiosApp app version 4 - global-instance file - Setting global instance for productsUrl, Setting global instance for randomUserUrl, this las two are defferent servers with global instance set

--- src/tutorial/14-axios-react-practice/setup/examples/4-global-instance.js
+++ src/tutorial/14-axios-react-practice/setup/examples/4-global-instance.js
@@ -1,11 +1,36 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
+/**AxiosApp app version 4 - 'global-instance' file - 
+ * Features:
+ * 
+ *              --> Setting 'global' js instance for
+ *                  'productsUrl'.          
+ *
+ *              --> Setting 'global' js instance for
+ *                  'randomUserUrl'.
+ * 
+ * Notes: 'randomUserUrl' and 'productsUrl' are two
+ * different servers and using global instance i set
+ * global instance for both.
+ * 
+ * This can be kind an issue with 'tokens' keys but
+ * also has a solution as custom-instance. 
+ * 
+ */
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const response1 = await axios(productsUrl);
+      const response2 = await axios(randomUserUrl)
+      console.log(response1)
+      console.log(response2)
+    } catch (error) {
+      
+    }
   };
 
   useEffect(() => {
