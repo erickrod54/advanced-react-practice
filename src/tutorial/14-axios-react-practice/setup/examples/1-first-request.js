c36a6d98b2222c5ae9813c204020386764c8bca2
Erick Rodriguez <erickrod54@gmail.com>
2022-04-23 20:49:34 -0400

    Axios app version 2 - axios-app file - Features: Building get request using axios

--- src/tutorial/14-axios-react-practice/setup/examples/1-first-request.js
+++ src/tutorial/14-axios-react-practice/setup/examples/1-first-request.js
@@ -1,10 +1,40 @@
 import { useEffect } from 'react';
-// limit, if 429 wait for 15 min and try again
+import axios from 'axios';
+
+/**AxiosApp app version 2 - 'axios-app' file - Features:
+ * 
+ *              --> Building 'get' request using axios.
+ * 
+ * Notes: The behavior is like a Promise, but the difference is
+ * that using axios i can get a big object with extended props
+ * that 'data' and i can checkout the object by 
+ * 'console.log(response)'.
+ */
+
+ /**limit, if 429 wait for 15 min and try again */
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    /**i use a try-catch */
+    try {
+      /**here i set the response */
+      const response = await axios(url);
+      /**here i can get the whole 'response' 
+       * console.log(response)
+      */
+
+      /**here i get from the object the 'data' prop, to get only
+       * the data*/
+      const data = response.data;
+      console.log(data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  /**here i implement 'useEffect' to invoke 'fetchData'*/
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
