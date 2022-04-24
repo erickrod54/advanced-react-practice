3c385d15e0c6bbc15bc1b0ecf5e8af8f0f61f4f5
Erick Rodriguez <erickrod54@gmail.com>
2022-04-24 12:19:00 -0400

    AxiosApp app version 3 - post-request file - Features: Implementing axios post method to send data, Using axios second argument to send name and email data

--- src/tutorial/14-axios-react-practice/setup/examples/3-post-request.js
+++ src/tutorial/14-axios-react-practice/setup/examples/3-post-request.js
@@ -2,13 +2,37 @@ import { useState } from 'react';
 import axios from 'axios';
 const url = 'https://course-api.com/axios-tutorial-post';
 
+/**AxiosApp app version 3 - 'post-request' file - Features:
+ * 
+ *              --> Implementing 'axios' post method to
+ *                  send 'data'.
+ *
+ *              --> Using axios second argument to send 
+ *                  'name' and 'email' data.
+ * 
+ * Notes: In order to checkout the 'post' payload i go
+ * to Chrome 'DevTools' > 'Network' > 'axios.post' > 
+ * 'here i can check the payload and also the sattus and 
+ * many more props'.
+ * 
+ * Here is not storing in the 'database' but is almost the
+ * same concept.
+ * 
+ */
 const PostRequest = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+      const response = await axios.post(url, {
+       name: name, email: email   
+      })
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response)
+    }
   };
 
   return (
