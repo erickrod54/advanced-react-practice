25e02efe16ed73cb51321e0856d8e24fadf9feef
Erick Rodriguez <erickrod54@gmail.com>
2022-04-25 17:06:01 -0400

    AxiosApp  version 5 - custom-instance file - Features: Importing AuthFetch custom instance , Setting custom js instance for productsUrl, Setting up global js instance for randomUserUrl

--- src/tutorial/14-axios-react-practice/setup/examples/5-custom-instance.js
+++ src/tutorial/14-axios-react-practice/setup/examples/5-custom-instance.js
@@ -1,10 +1,37 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/custom';
+import axios from 'axios';
+
+/**AxiosApp app version 5 - 'custom-instance' file - 
+ * Features:
+ * 
+ *              --> Importing 'AuthFetch' custom instance.
+ *  
+ *              --> Setting 'custom' js instance for
+ *                  'productsUrl'.          
+ *
+ *              --> Setting 'global' js instance for
+ *                  'randomUserUrl'.
+ * 
+ * Notes: 'randomUserUrl' and 'productsUrl' are two
+ * different servers and using global instance.
+ * 
+ * Setting a custom instance is a solution to handle
+ * 'tokens' beceause the get request can be customized.
+ */
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const response1 = await authFetch('/react-store-products')
+      const response2 = await axios(randomUserUrl)
+      console.log(response1)
+      console.log(response2)
+    } catch (error) {
+      
+    }
   };
 
   useEffect(() => {
