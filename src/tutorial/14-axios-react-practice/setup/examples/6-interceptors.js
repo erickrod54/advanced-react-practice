2b7c4c0c28d5ef19231d156a9e3cbc7cae801f3f
Erick Rodriguez <erickrod54@gmail.com>
2022-04-27 20:48:44 -0400

    AxiosApp app version 6 - interceptors file - Features: Importing authFetch instance

--- src/tutorial/14-axios-react-practice/setup/examples/6-interceptors.js
+++ src/tutorial/14-axios-react-practice/setup/examples/6-interceptors.js
@@ -1,10 +1,35 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
+
+/**AxiosApp app version 6 - 'interceptors' file - 
+ * Features:
+ * 
+ *              --> Importing 'authFetch' instance
+ * 
+ * Notes: 'randomUserUrl' and 'productsUrl' are two
+ * different servers and using global instance.
+ * 
+ * Interceptors are functions that axios call for every 
+ * request and i can use it to transform the request 
+ * before it leaves the application 
+ * 
+ * This is handy for complex application that need to 
+ * handle mixed features with authentication.
+ */
 
 const url = 'https://course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      /**i can test with routes that doesn't exist
+       * to see how is the error handled
+       */
+      const response = authFetch('/react-store-products')
+      console.log(response)
+    } catch (error) {
+      
+    }
   };
 
   useEffect(() => {
